16fc3b2d3c3965b993b501c167ad438dc8b29121
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-10-16 07:48:08 +0400

    feat: add test <should add two numbers>

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,68 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const numbersTestCases = [
+      {
+        a: 1,
+        b: 2,
+        action: Action.Add
+      },
+      {
+        a: 10,
+        b: 3,
+        action: Action.Add
+      },
+      {
+        a: -2,
+        b: 4,
+        action: Action.Add
+      },
+      {
+        a: 4,
+        b: 27,
+        action: Action.Add
+      },
+      {
+        a: 13,
+        b: 9,
+        action: Action.Add
+      },
+      {
+        a: 8,
+        b: 4,
+        action: Action.Add
+      },
+    ];
+
+    numbersTestCases.forEach(({a, b, action}) => {
+      const result = simpleCalculator({a, b, action});
+      return expect(result).toBe(a + b);
+    })
   });
 
-  test('should subtract two numbers', () => {
-    // Write your test here
-  });
+  // test('should subtract two numbers', () => {
+  //   // Write your test here
+  // });
 
-  test('should multiply two numbers', () => {
-    // Write your test here
-  });
+  // test('should multiply two numbers', () => {
+  //   // Write your test here
+  // });
 
-  test('should divide two numbers', () => {
-    // Write your test here
-  });
+  // test('should divide two numbers', () => {
+  //   // Write your test here
+  // });
 
-  test('should exponentiate two numbers', () => {
-    // Write your test here
-  });
+  // test('should exponentiate two numbers', () => {
+  //   // Write your test here
+  // });
 
-  test('should return null for invalid action', () => {
-    // Write your test here
-  });
+  // test('should return null for invalid action', () => {
+  //   // Write your test here
+  // });
 
-  test('should return null for invalid arguments', () => {
-    // Write your test here
-  });
+  // test('should return null for invalid arguments', () => {
+  //   // Write your test here
+  // });
 });
